a27dff9fa13f76e7037cdd9d7824bcd785a29aad
Jarod Wilson <jaroddwilson@gmail.com>
2022-09-02 21:23:21 -0400

    commit tree f973ea755fbfc644d83c6a79fcb6e1994462258f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  last_word = array.pop
+  return last_word if array == []
+  return "#{array[0]} and #{last_word}" if array.size == 1
+  array.join(", ") + ", and #{last_word}"
 end
+
+# def oxford_comma(array)
+#   last_word = array.pop
+#   penultimate_word = array.pop if array != []
+#   return array.join(", ") + ", #{penultimate_word}, and #{last_word}" if array != []
+#   return "#{penultimate_word} and #{last_word}" if penultimate_word != nil
+#   last_word
+# end
